916f4ddae1175d6d8df16df5a1591213954272aa
SukritiC <sukriti0892@gmail.com>
2021-10-25 00:46:50 +0530

    Python CP

---
+++ find_substring.py
@@ -0,0 +1,13 @@
+def count_substring(string, sub_string):
+    count = 0
+    for i in range(0,len(string)):
+        if sub_string[0:1] in string:
+            # print(i)
+            string = string[string.find(sub_string):len(string)]
+            # print(string)
+        if sub_string in string:
+            count  += 1
+        string = string[1:]
+    return count
+
+if __name__ == '__main__':
