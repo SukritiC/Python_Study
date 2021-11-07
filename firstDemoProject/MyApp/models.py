8ab3a1bd44f6cdc3c9fa99641ebd97674765523a
SukritiC <sukriti0892@gmail.com>
2021-11-07 16:59:16 +0530

    Adding __str__ function and admin panel for adding objects to the model

--- firstDemoProject/MyApp/models.py
+++ firstDemoProject/MyApp/models.py
@@ -4,6 +4,10 @@ from django.db import models
 
 class  Book(models.Model):
 
+    # A string representation of a model -
+    def __str__(self):
+        return self.name
+
     name = models.CharField(max_length = 100)
     desc = models.CharField(max_length = 300)
     price = models.IntegerField()
