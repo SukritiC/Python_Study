cafbceba6a58e626e93599895cec9cd1f24651ba
SukritiC <sukriti0892@gmail.com>
2021-11-07 23:48:32 +0530

    Adding Template

--- firstDemoProject/MyApp/views.py
+++ firstDemoProject/MyApp/views.py
@@ -1,12 +1,28 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Book
 # Create your views here.
 
 
 # the function must accept request and give a response- django view
 def index(request):
     # view must return a proper HTTP response - hence imported the above HttpResponse
-    return HttpResponse('Hello World')
+    # return HttpResponse('Hello World')
+
+    # passing the objects to the view
+    list_books = Book.objects.all()
+    # return HttpResponse(list_books)
+
+    # creating context to pass objects to HTML page
+    context = {
+        'book_list':list_books
+    }
+
+    # rendering the template
+    return render(request,'MyApp/index.html',context)
+
+
+
 
 
 # creating a view isn't sufficient - it must be associated with a URL pattern
